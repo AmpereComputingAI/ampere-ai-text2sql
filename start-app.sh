617c336c41fa40fbc9bc2c54ded354bcbb4023e6
Iyappan Subramanian <isubramanian@amperecomputing.com>
2025-12-06 00:11:57 +0000

    Added ollama pull model command

--- start-app.sh
+++ start-app.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker compose up --remove-orphans -d
+
+docker exec -it ollama bash -c 'while ! curl -s http://localhost:11434 > /dev/null; do echo "Waiting for Ollama server..."; sleep 2; done; echo "Ollama server is running. Pulling model..."; ollama pull hf.co/mradermacher/Arctic-Text2SQL-R1-7B-GGUF:Arctic-Text2SQL-R1-7B.Q8_0.gguf'
